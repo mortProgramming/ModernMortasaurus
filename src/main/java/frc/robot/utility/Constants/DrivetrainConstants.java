d62aee37436e676544ca107a58028fbde9cdf54e
Drive Station1 <programming@mort11.org>
2024-05-16 16:15:11 -0400

    all drivetrain ids

--- src/main/java/frc/robot/utility/Constants/DrivetrainConstants.java
+++ src/main/java/frc/robot/utility/Constants/DrivetrainConstants.java
@@ -1,9 +1,9 @@
 package frc.robot.utility.Constants;
 
 public class DrivetrainConstants {
-    public static final int LEFT_MASTER_PORT = 0;
-    public static final int RIGHT_MASTER_PORT = 0;
-    public static final int LEFT_FOLLOWER_PORT = 0;
-    public static final int RIGHT_FOLLOWER_PORT = 0;
+    public static final int LEFT_MASTER_PORT = 1;
+    public static final int RIGHT_MASTER_PORT = 2;
+    public static final int LEFT_FOLLOWER_PORT = 3;
+    public static final int RIGHT_FOLLOWER_PORT = 4;
     
 }
